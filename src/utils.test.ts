34f9b1576610f0d1b5f3a69c1b2d5df6ee6fcfad
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-11-19 15:08:19 -0300

    test: add test to 'calculateAmountToPay' function

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,18 +1,46 @@
+import { bankSlip } from "./bankSlip";
 import { utils } from "./utils";
 
 describe("Utils tests", function () {
-  // Calcular o período em meses
+  // Calcular o período em meses (driver)
   test("periodInMonths: 1 month", () => {
     const initialDate = new Date("2023-09-15");
     const finalDate = new Date("2023-10-15");
-    const resultado = utils.periodInMonths(initialDate, finalDate);
-    expect(resultado).toEqual(1);
+    const result = utils.periodInMonths(initialDate, finalDate);
+    expect(result).toEqual(1);
   });
 
+  // Calcular o período em meses (driver)
   test("periodInMonths: 1.5 month", () => {
     const initialDate = new Date("2023-09-15");
     const finalDate = new Date("2023-10-30");
-    const resultado = utils.periodInMonths(initialDate, finalDate);
-    expect(resultado).toEqual(1.5);
+    const result = utils.periodInMonths(initialDate, finalDate);
+    expect(result).toEqual(1.5);
+  });
+});
+
+describe("BankSlipt tests", function () {
+  // Calcular o valor a pagar (driver)
+  test("calculateAmountToPay: without delay", () => {
+    const expirationDate = new Date("2023-09-15");
+    const paymentDate = new Date("2023-09-10");
+    const result = bankSlip.calculateAmountToPay(
+      500,
+      expirationDate,
+      paymentDate,
+    );
+    expect(result).toEqual(500);
+  });
+
+  // Calcular o valor a pagar (driver)
+  test("calculateAmountToPay: with 1 month delay", () => {
+    const expirationDate = new Date("2023-09-15");
+    const paymentDate = new Date("2023-10-15");
+    const result = bankSlip.calculateAmountToPay(
+      500,
+      expirationDate,
+      paymentDate,
+    );
+    expect(result).toEqual(505);
   });
 });
